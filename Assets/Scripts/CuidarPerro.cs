f0d8997ccf1ee55b8c53cd6dda120fa91383dcdf
Carrasco <136844452+Cocom1n@users.noreply.github.com>
2024-11-18 15:30:52 -0300

    Botones mandan mensaje por consola
    asdas

---
+++ Assets/Scripts/CuidarPerro.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuidarPerro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Alimentar()
+    {
+        Debug.Log("Perro come jaja");
+    }
+
+    public void Bañar()
+    {
+        Debug.Log("Limpioo");
+    }
+
+    public void Curar()
+    {
+        Debug.Log("Perro sano");
+    }
+
+    public void Acariciar()
+    {
+        Debug.Log("Buen perrito");
+    }
+}
